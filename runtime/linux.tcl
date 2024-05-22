0adb3d326923b5f9956264e27856efea7d9b5905
Loïc Daudé Mondet <loic.daude@mondet.fr>
2024-05-22 22:47:05 +0200

    adding more logging

--- runtime/linux.tcl
+++ runtime/linux.tcl
@@ -596,8 +596,11 @@ proc createNodeQemu { node } {
     set iso [getNodeqemuIso $node]
     set kvm [getNodeqemuKvm $node]
   if {$bootType == 0} {
+    puts "qemu-system-x86_64 $kvm -m $memory -hda $image -nic tap -display none -vga qxl -vnc :0 -k fr -daemonize -qmp unix:/tmp/qmp-sock,server,wait=off"
     catch { exec qemu-system-x86_64 $kvm -m $memory -hda $image -nic tap -display none -vga qxl -vnc :0 -k fr -daemonize -qmp unix:/tmp/qmp-sock,server,wait=off}
   } else {
+    puts " qemu-system-x86_64 $kvm -m $memory -hda $image -nic tap -display none -vga qxl -vnc :0 -k fr -daemonize -qmp unix:/tmp/qmp-sock,server,wait=off -cdrom $iso -boot order=d
+"
 
     catch { exec qemu-system-x86_64 $kvm -m $memory -hda $image -nic tap -display none -vga qxl -vnc :0 -k fr -daemonize -qmp unix:/tmp/qmp-sock,server,wait=off -cdrom $iso -boot order=d
   }
