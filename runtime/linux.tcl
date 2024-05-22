fdecbd8638eb7094a81ef20601b8c24ba699acca
Loïc Daudé Mondet <loic.daude@mondet.fr>
2024-05-22 18:25:47 +0200

    trying to launch qemu via imunes

--- runtime/linux.tcl
+++ runtime/linux.tcl
@@ -593,7 +593,7 @@ proc createNodeQemu { node } {
 
     set image "/root/alpine.qcow2"
 
-    catch { exec qemu-system-x86_64 -m 1024 -hda $image -accel kvm -nic tap} 
+    catch { exec qemu-system-x86_64 -m 1024 -hda $image -accel kvm -nic tap -display none -vga qxl -vnc :0 -k fr &} 
 
 
 }
